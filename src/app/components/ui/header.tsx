b1d204d705259e16c9e218fdd13e8a37919c3aed
Shrimpey304 <124566452+Shrimpey304@users.noreply.github.com>
2025-03-01 17:58:09 +0100

    feat: added various pages to header

--- src/app/components/ui/header.tsx
+++ src/app/components/ui/header.tsx
@@ -8,34 +8,35 @@ interface RedirectButtonProps {
 
 export default function Header() {
     return (
-        <div className="header flex items-center space-x-4 border-b-2 border-maintheme">
+        <div className="flex items-center space-x-3 pr-3 border-b-2 border-maintheme">
             <DisplayLogo />
             <RedirectButton 
                 text="Home" 
                 redirect="/" 
             />
             <RedirectButton 
-                text="Home 2" 
-                redirect="/" 
+                text="Analytics" 
+                redirect="/analytics"
             />
             <RedirectButton 
-                text="Home 3" 
-                redirect="/" 
+                text="Settings" 
+                redirect="/settings"
             />
-            
+            <div className="flex-grow"></div>
+            <DisplayUserProfile />
         </div>
     );
 }
 
 function DisplayLogo() {
     return (
-        <div className="logo">
+        <div className="w-13 h-13">
             <Link href="/">
                 <Image 
                     src="/logo.svg"
                     alt="logo"
-                    width={130}
-                    height={130}
+                    width={100}
+                    height={100}
                 />
             </Link>
         </div>
@@ -44,12 +45,29 @@ function DisplayLogo() {
 
 function RedirectButton({ text, redirect }: RedirectButtonProps) {
     return (
-        <div className="redirect-button">
-            <a href={redirect}>
-                <button className="px-4 py-2 border-2 border-maintheme text-main-color rounded-md hover:bg-main-color hover:text-maintheme transition duration-200">
+        <div className="px-2 py-1 border-2 border-maintheme text-main-color rounded-md ">
+            <Link href={redirect}>
+                <button className="hover:bg-main-color hover:text-maintheme transition duration-200">
                     {text}
                 </button>
-            </a>
+            </Link>
+        </div>
+    );
+}
+
+function DisplayUserProfile() {
+    return (
+        <div className="bg-maintheme rounded-full">
+            <Link href="/profile">
+                <div className="bg-maintheme rounded-full">
+                    <Image 
+                        src="/generic_user_profile.webp"
+                        alt="profile"
+                        width={50}
+                        height={50}
+                    />
+                </div>
+            </Link>
         </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import Header from './components/ui/header';
-import RootLayout from './layout';
 
 export default function Home() {
   return (
-    <RootLayout>
-      <Header />
-    </RootLayout>
+    <div>
+      <p>Home</p>
+    </div>
   );
 }
